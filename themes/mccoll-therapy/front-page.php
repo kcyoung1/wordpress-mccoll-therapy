dbceb6fd83c79e43b50410f5cf95d3ceb5b1aade
Kurtis Young <kcyoung1@ualberta.ca>
2016-08-04 16:46:00 -0700

    add banner images to the assets folder, create HTML mark up for the hero banner background

--- themes/mccoll-therapy/front-page.php
+++ themes/mccoll-therapy/front-page.php
@@ -10,6 +10,13 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<section class="banner-container">
+				<div class="hero-banner">
+					<h1>McColl Therapy</h1>
+					<h3>Support for a lasting wellness</h3>
+				</div>
+			</section><!-- .banner-container -->
+
 
 
 		</main><!-- #main -->
