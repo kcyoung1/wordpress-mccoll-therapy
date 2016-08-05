90514205fe1960f5b1dc7e6c63aab832d201466e
Kurtis Young <kcyoung1@ualberta.ca>
2016-08-04 17:18:08 -0700

    create outline for the website, where each section goes on the single web page

--- themes/mccoll-therapy/front-page.php
+++ themes/mccoll-therapy/front-page.php
@@ -1,4 +1,4 @@
-<?php
+2<?php
 /**
  * The Front Page.
  *
@@ -13,11 +13,64 @@ get_header(); ?>
 			<section class="banner-container">
 				<div class="hero-banner">
 					<h1>McColl Therapy</h1>
-					<h3>Support for a lasting wellness</h3>
+					<h2>Support for a lasting wellness</h2>
 				</div>
 			</section><!-- .banner-container -->
 
+			<section class="about-container">
+				<h2>About Us</h2>
+				<p>
+					Custom post type possibly
+					About Us company overview - Who We Are - supporting [balance and growth / independence and resilience / ???] (horizontal side scroll? Currently mission, values etc. should be minimal text, ~3 statements)
+				</p>
+			</section>
 
+			<section class="profile-container">
+				<h2>Profile</h2>
+				<p>
+					Custom post type possibly
+					Specific About Us - circle headshots of each practitioner associated, name, designation and titles under - click each headshot for a pop-up page - half body shot (cutoff lower legs), background, bio, specializations
+				</p>
+			</section>
+
+			<section class="graphic-one-container">
+				<h2>Infographic Part 1</h2>
+				<p>
+					Hardcode
+					Infographic part 1 - top, each wellness dimension, individual circle, title underneath - click icon for pop-up page, additional info and very general recommendations - underneath, left shifted logo wellness diagram - right shifted explanation of diagram
+				</p>
+			</section>
+
+			<section class="graphic-two-container">
+				<h2>Infographic Part 2</h2>
+				<p>
+					Icons
+					Infographic part 2 - top text, links importance of  maintaining and growing in important aspects of wellness allows people to handle challenges and live meaningfully - underneath, icons, click each icon for pop-up page, additional info and very general recommendations
+				</p>
+			</section>
+
+			<section class="services-container">
+				<h2>Services</h2>
+				<p>
+					Icons
+					Services - all visible or side scroll - icons and name and/or with taglines/hooks if sidescroll - Click for pop-up page with description of service
+				</p>
+			</section>
+
+			<section class="information-container">
+				<h2>Information</h2>
+				<p>
+					Link
+					Information and resources for support (links to financial aid, free local resources eg crisis number, support network number, 211(? because they offer counselling too))
+				</p>
+			</section>
+
+			<section class="contact-container">
+				<h2>Contact Us</h2>
+				<p>
+					Contact form 7 with call to action
+				</p>
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
