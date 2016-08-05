13fda5537ee9d16c28c12719c1e6fa10b8bac03e
Kurtis Young <kcyoung1@ualberta.ca>
2016-08-04 17:33:59 -0700

    add a container to the secitons and media query mixins for tablet and desktop view

--- themes/mccoll-therapy/front-page.php
+++ themes/mccoll-therapy/front-page.php
@@ -10,67 +10,67 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="banner-container">
+			<section class="banner-wrapper">
 				<div class="hero-banner">
 					<h1>McColl Therapy</h1>
 					<h2>Support for a lasting wellness</h2>
 				</div>
-			</section><!-- .banner-container -->
+			</section><!-- .banner-wrapper -->
 
-			<section class="about-container">
+			<section class="about-wrapper container">
 				<h2>About Us</h2>
 				<p>
 					Custom post type possibly
 					About Us company overview - Who We Are - supporting [balance and growth / independence and resilience / ???] (horizontal side scroll? Currently mission, values etc. should be minimal text, ~3 statements)
 				</p>
-			</section>
+			</section><!-- .about-wrapper -->
 
-			<section class="profile-container">
+			<section class="profile-wrapper container">
 				<h2>Profile</h2>
 				<p>
 					Custom post type possibly
 					Specific About Us - circle headshots of each practitioner associated, name, designation and titles under - click each headshot for a pop-up page - half body shot (cutoff lower legs), background, bio, specializations
 				</p>
-			</section>
+			</section><!-- .profile-wrapper -->
 
-			<section class="graphic-one-container">
+			<section class="graphic-one-wrapper container">
 				<h2>Infographic Part 1</h2>
 				<p>
 					Hardcode
 					Infographic part 1 - top, each wellness dimension, individual circle, title underneath - click icon for pop-up page, additional info and very general recommendations - underneath, left shifted logo wellness diagram - right shifted explanation of diagram
 				</p>
-			</section>
+			</section><!-- .graphic-one-wrapper -->
 
-			<section class="graphic-two-container">
+			<section class="graphic-two-wrapper container">
 				<h2>Infographic Part 2</h2>
 				<p>
 					Icons
 					Infographic part 2 - top text, links importance of  maintaining and growing in important aspects of wellness allows people to handle challenges and live meaningfully - underneath, icons, click each icon for pop-up page, additional info and very general recommendations
 				</p>
-			</section>
+			</section><!-- .graphic-two-wrapper -->
 
-			<section class="services-container">
+			<section class="services-wrapper container">
 				<h2>Services</h2>
 				<p>
 					Icons
 					Services - all visible or side scroll - icons and name and/or with taglines/hooks if sidescroll - Click for pop-up page with description of service
 				</p>
-			</section>
+			</section><!-- .services-wrapper -->
 
-			<section class="information-container">
+			<section class="information-wrapper container">
 				<h2>Information</h2>
 				<p>
 					Link
 					Information and resources for support (links to financial aid, free local resources eg crisis number, support network number, 211(? because they offer counselling too))
 				</p>
-			</section>
+			</section><!-- .information-wrapper -->
 
-			<section class="contact-container">
+			<section class="contact-wrapper  container">
 				<h2>Contact Us</h2>
 				<p>
 					Contact form 7 with call to action
 				</p>
-			</section>
+			</section><!-- .contact-wrapper -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
