f898f24928a3aa1a1df07e18f0ff7dadfe58bf50
Kurtis Young <kcyoung1@ualberta.ca>
2016-08-07 19:15:31 -0700

    add the smooth scroll jquery code to the scripts.js file

--- themes/mccoll-therapy/functions.php
+++ themes/mccoll-therapy/functions.php
@@ -87,6 +87,8 @@ function mccoll_therapy_scripts() {
 
 	wp_enqueue_script( 'mccoll-therapy-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
+		wp_enqueue_script( 'scripts-js', get_template_directory_uri() . '/build/js/scripts.min.js', array('jquery'), false, true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
---
+++ themes/mccoll-therapy/js/scripts.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function() {
+    // Smooth Scrolling
+
+    jQuery('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = jQuery(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                jQuery('html,body').animate({
+                    scrollTop: target.offset().top - 70
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
