6b82bb0584678a8ff02d1793cf8031a758240eda
Kurtis Young <kcyoung1@ualberta.ca>
2016-11-20 13:19:02 -0800

    remove unused icons and add two new icons

--- themes/mccoll-therapy/template-parts/content-wellness.php
+++ themes/mccoll-therapy/template-parts/content-wellness.php
@@ -17,87 +17,103 @@
   <div class="dimensions-wrapper">
     <ul class="accordian ">
       <li class="accordian-content emotional-wrapper">
+        <div class="border"></div>
         <div class="accordian-head">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/dw-emotional.svg" alt="Emotional Logo" />
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/dw-emotional.png" alt="Emotional Logo" />
           <h2>Emotional</h2>
           <button type="button" name="button">+</button>
         </div>
+        <div class="border"></div>
         <div class="expand-content">
-          <p>
-            Emotional Wellness entails awareness, expression and acceptance of our feelings. Developing our emotional wellness allows us to be more in-tune with ourselves, to handle difficult emotions, and to foster connections with others. Examples of ways to support Emotional Wellness include:
-          </p>
-          <ul>
-              <li>Being present and focused in the moment</li>
-              <li>Participating in enjoyable and meaningful activities</li>
-              <li>Developing effective coping strategies</li>
-              <li>Maintaining a non-judgemental perspective</li>
-          </ul>
-          <a href="#contact" class="btn">Contact Us</a>
+          <div class="content-wrap">
+            <p>
+              Emotional Wellness entails awareness, expression and acceptance of our feelings. Developing our emotional wellness allows us to be more in-tune with ourselves, to handle difficult emotions, and to foster connections with others. Examples of ways to support Emotional Wellness include:
+            </p>
+            <ul>
+                <li>Being present and focused in the moment</li>
+                <li>Participating in enjoyable and meaningful activities</li>
+                <li>Developing effective coping strategies</li>
+                <li>Maintaining a non-judgemental perspective</li>
+            </ul>
+            <a href="#contact" class="btn">Contact Us</a>
+          </div>
         </div>
       </li>
       <li class="accordian-content cognitive-wrapper">
+        <div class="border"></div>
         <div class="accordian-head ">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/dw-mental.svg" alt="" />
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/dw-cognitive.png" alt="" />
           <h2>Cognitive</h2>
           <button type="button" name="button">+</button>
         </div>
+        <div class="border"></div>
         <div class="expand-content">
-          <p>
-            Cognitive Wellness entails self-awareness, open-mindedness, and effective problem solving. Developing our Cognitive Wellness allows us to live productively and overcome challenges. Examples of ways to support Cognitive Wellness include:
-          </p>
-          <ul>
-              <li>Recognizing unbalanced thought patterns</li>
-              <li>Considering facts, evidence and alternative perspectives</li>
-              <li>Engaging in education and career pursuits</li>
-              <li>Planning and preparing for difficult situations</li>
-          </ul>
-          <a href="#contact" class="btn">Contact Us</a>
+          <div class="content-wrap">
+            <p>
+              Cognitive Wellness entails self-awareness, open-mindedness, and effective problem solving. Developing our Cognitive Wellness allows us to live productively and overcome challenges. Examples of ways to support Cognitive Wellness include:
+            </p>
+            <ul>
+                <li>Recognizing unbalanced thought patterns</li>
+                <li>Considering facts, evidence and alternative perspectives</li>
+                <li>Engaging in education and career pursuits</li>
+                <li>Planning and preparing for difficult situations</li>
+            </ul>
+            <a href="#contact" class="btn">Contact Us</a>
+          </div>
         </div>
       </li>
       <li class="accordian-content physical-wrapper">
+        <div class="border"></div>
         <div class="accordian-head">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/dw-physical.svg" alt="" />
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/dw-physical.png" alt="" />
           <h2>Physical</h2>
           <button type="button" name="button">+</button>
         </div>
+        <div class="border"></div>
         <div class="expand-content">
-          <p>
-            Physical Wellness entails attending to our bodies’ needs and maintaining a healthy lifestyle. Developing our Physical Wellness protects us from chronic health issues and reduces our vulnerabilities to imbalances across other dimensions of wellness. Examples of ways to support Physical Wellness include:
-        </p>
-        <ul>
-            <li>Keeping a balanced and nutritious diet</li>
-            <li>Engaging in physical activity and exercise</li>
-            <li>Maintaining a regular sleep schedule</li>
-            <li>Seeking medical help when needed</li>
-        </ul>
-        <a href="#contact" class="btn">Contact Us</a>
+          <div class="content-wrap">
+            <p>
+              Physical Wellness entails attending to our bodies’ needs and maintaining a healthy lifestyle. Developing our Physical Wellness protects us from chronic health issues and reduces our vulnerabilities to imbalances across other dimensions of wellness. Examples of ways to support Physical Wellness include:
+          </p>
+          <ul>
+              <li>Keeping a balanced and nutritious diet</li>
+              <li>Engaging in physical activity and exercise</li>
+              <li>Maintaining a regular sleep schedule</li>
+              <li>Seeking medical help when needed</li>
+          </ul>
+          <a href="#contact" class="btn">Contact Us</a>
+          </div>
         </div>
       </li>
       <li class="accordian-content spiritual-wrapper">
+        <div class="border"></div>
         <div class="accordian-head">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/dw-spiritual.svg" alt="" />
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/dw-spiritual.png" alt="" />
           <h2>Spiritual</h2>
           <button type="button" name="button">+</button>
         </div>
+        <div class="border"></div>
         <div class="expand-content">
-          <p>
-              Spiritual Wellness entails making decisions and acting in line with personal values and beliefs. Developing our Spiritual Wellness allows us to nurture self-esteem and to pursue rewarding experiences in life. Examples of ways to support Spiritual Wellness include:
-          </p>
-          <ul>
-              <li>	Fostering connections with the people we care about</li>
-              <li>Supporting our communities and causes</li>
-              <li>Identifying and maintaining personal boundaries</li>
-              <li>Being open-minded about our experiences
-             nulla.</li>
-          </ul>
-          <a href="#contact" class="btn">Contact Us</a>
+          <div class="content-wrap">
+            <p>
+                Spiritual Wellness entails making decisions and acting in line with personal values and beliefs. Developing our Spiritual Wellness allows us to nurture self-esteem and to pursue rewarding experiences in life. Examples of ways to support Spiritual Wellness include:
+            </p>
+            <ul>
+                <li>	Fostering connections with the people we care about</li>
+                <li>Supporting our communities and causes</li>
+                <li>Identifying and maintaining personal boundaries</li>
+                <li>Being open-minded about our experiences
+               nulla.</li>
+            </ul>
+            <a href="#contact" class="btn">Contact Us</a>
+          </div>
         </div>
       </li>
     </ul>
   </div>
 
   <div class="dw-diagram container">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/dimensions.jpg" alt="Dimensions Diagram" />
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/dimensions.png" alt="Dimensions Diagram" />
     <div class="diagram-info align-center">
         Wellness entails supporting these different dimensions as they affect one’s quality of life. There are many ways to support personal growth and lasting wellness, and McColl Therapy assists you across these dimensions through an individualized process.
     </div>
